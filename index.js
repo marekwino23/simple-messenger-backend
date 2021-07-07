76e1ce87e26d4aa02851df380ea1f0dd711d1f96
marekwino23 <marekw1996@gmail.com>
2021-07-07 19:12:28 +0200

    setup simple_messenger

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app)
-const io = require("socket.io")(server, { cors: { origin:"http://localhost:3000"} });
-const fs = require('fs');
+const io = require("socket.io")(server, { cors: { origin: '*'} });
 const { json } = require("express");
+
+const messages = [];
 
 app.use(express.json())
 
@@ -19,8 +20,9 @@ io.on("connection", (socket) => {
         console.log(`${user} joined room ${data}`)
     });
     socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data.content )
-        console.log(data)
+        messages.push(data);
+        console.log("response: ", messages);
+        io.in(data.room).emit("receive_message", messages );
     })
     socket.on("disconnect", () => {
         console.log("USER DISCONNECT")
